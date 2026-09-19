a36aa7629acfaa6671e1f1af87bf4cf0319bf06b
Bandaru Dheeraj <139301855+Dheeraj2834@users.noreply.github.com>
2026-09-19 09:46:49 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevops"
+    bucket       = "dheerajdheeraj"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
